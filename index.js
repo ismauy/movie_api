49518c0f41cc83650ed8bac7dc99e2fd49d16a55
ismauy <isma.ferrazzi@gmail.com>
2021-09-30 21:27:06 +0200

    Express

---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require('express'),
+      morgan = require('morgan');
+const app = express();
+
+
+let topMovies = [
+  {
+    title: 'Harry Potter and the Sorcerer\'s Stone',
+  },
+  {
+    title: 'Lord of the Rings',
+  },
+  {
+    title: 'Twilight',
+  },
+  {
+    title: 'The Godfather',
+  },
+  {
+    title: 'Back to the future',
+  },
+  {
+    title: 'Titanic',
+  },
+  {
+    title: 'Manyas',
+  },
+  {
+    title: '25 Watts',
+  },
+  {
+    title: 'Pulp Fiction',
+  },
+  {
+    title: 'The Wall',
+  }
+];
+
+app.use(morgan('common'));
+
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Houston, we have a problem!');
+});
+
+
+
+// GET requests
+app.get('/', (req, res) => {
+  res.send('Welcome to myFlix app!');
+});
+
+app.get('/documentation', (req, res) => {
+  res.sendFile('public/documentation.html', { root: __dirname });
+});
+
+app.get('/movies', (req, res) => {
+  res.json(topMovies);
+});
+
+
+// listen for requests
+app.listen(8080, () => {
+  console.log('Your app is listening on port 8080.');
+});
