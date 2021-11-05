0e07af162957c4e60a5801995a3127d80957b86e
ismauy <isma.ferrazzi@gmail.com>
2021-11-05 22:40:07 +0100

    conection URI variable

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ const Users = Models.User;
 
 
 
-mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 app.use(bodyParser.json());
